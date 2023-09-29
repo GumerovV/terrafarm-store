2a82c5a60b0fcf22278f1d16b844f6091bbcce13
Admin <gumerov.vlad20034@gmail.com>
2023-09-29 17:35:38 +0700

    This is a new commit for what I originally planned to be amended

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
-import Header from "./components/pageHeader/Header";
-import Home from "./components/screens/home/Home";
-import { Routes, Route} from "react-router-dom";
-import ClothesItemDetail from "./components/screens/home/ClothesItemDetail/ClothesItemDetail";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return ( 
-        <>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/clothe/:id" element={<ClothesItemDetail />}/>
-            </Routes>
-        </>
-     );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
