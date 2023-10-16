043f95d16d81bd60edd55625b5e1aec0281953c9
Artyom <keloss94@mail.ru>
2023-10-16 11:59:34 +0700

    del feedback

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,9 +2,7 @@ import M1 from "../assets/M1.webp"
 import M2 from "../assets/M2.webp"
 import M3 from "../assets/M3.webp"
 import L1 from "../assets/L1.webp"
-import otz_tanya from "../assets/otz_tanya.jpeg"
-import otz_luk from "../assets/otz_luk.jpeg"
-import otz_katya from "../assets/otz_katya.jpg"
+
 
 function Products() {
     return (
@@ -31,6 +29,8 @@ function Products() {
             B-3
             <img className="max-w-full w-96 h-64 duration-0.2 delay-0 transition-opacity" src={M3} alt=""></img>
           </div>
+          <div>
+          </div>
           <div className="font-extralight inline-block relative truncate no-underline mt-3 mr-1.5 mb-0 ml-1.5 p-5">
             MINI
             <img className="max-w-full w-96 h-64 duration-0.2 delay-0 transition-opacity" src={M1} alt=""></img>
@@ -53,7 +53,6 @@ function Products() {
           <div className="font-extralight text-white text-2xl m-3">
           Лабораторное оборудование представляет из себя герметичные камеры широкого климатического диапазона. 
           Применяются для создания различных климатических условий, проведения опытов и экспериментов над растениями. 
-          Устройство способно воссоздать практически любые климатические условия на планете Земля, и даже близкие к другим планетам.
           </div>
           <div className="font-extralight inline-block relative truncate no-underline mt-3 mr-1.5 mb-0 ml-1.5 p-5">
             LITE
@@ -71,37 +70,28 @@ function Products() {
 
         <div className="text-white text-center my-1 mx-5 mb-0 ml-5">
           <div className = "text-center text-uppercase font-extralight pt-1 pl-5 pr-5 text-white text-4xl p-5">
-          ОТЗЫВЫ НАШИХ ПОКУПАТЕЛЕЙ
+          ГИДРОПОННЫЕ И АЭРОПОННЫЕ СИСТЕМЫ
           </div>
-          <hr className="mx-auto w-4/5 text-center color-white"/>
-
-          <div className="text-base mt-10 mb-10 mx-10 text-white text-center">
-
-            <div className="mt-0 mr-3 mb-5 ml-32 inline-block relative text-center align-top w-1/5">
-              <img className="rounded-full border-2 border-solid w-24" src={otz_tanya} alt=""/>
-              <div className="text-2xl italic">Гарнаева Татьяна</div>
-              <div className="italic text-center font-extralight text-xl">
-                Заказала «SmartPot Plus» и не жалею все работает отлично, без сбоев теперь можно уезжать в отпуск и не переживать за мою любимую Хавортию</div>
-            </div>
-
-            <div className="mt-0 mr-3 mb-5 ml-32 inline-block relative text-center align-top w-1/5">
-              <img className="rounded-full border-2 border-solid w-24" src={otz_luk} alt=""/>
-              <div className="text-2xl italic">Жуков Лукьян</div>
-                <div className="italic text-center font-extralight text-xl">
-                После проведения ремонта в квартире в стиле High-Tec, это устройство идеально вписалось в интерьер квартиры, a подсветка c туманом придает определенный шаарм
-                </div>
-            </div>
-
-            <div className="mt-0 mr-0 mb-5 ml-32 inline-block relative text-center align-top w-1/5">
-              <img className="rounded-full border-2 border-solid w-24" src={otz_katya} alt=""/>
-              <div className="text-2xl italic">Гуралевич Екатерина</div>
-                <div className="italic text-center font-extralight text-xl">
-                Подарила своей бабушке «SmartPot Series 3», она очень любит герань и здесь этот цветок идет в подарок, ваш горшок поможет ей больше не тратить сил на уход за геранью
-                </div>
-            </div>
-
+        <hr className="mx-auto w-4/5 text-center color-white"/>
+          <div className="font-extralight text-white text-2xl m-3">
+          Гидропонные и аэропонные системы это идеальный вариант для бизнеса, если необходимо максимально быстро и качественно выращивать продукты питания.
           </div>
         </div>
+        
+        <div>
+
+        </div>
+
+        <div className="text-white text-center my-1 mx-5 mb-0 ml-5">
+          <div className = "text-center text-uppercase font-extralight pt-1 pl-5 pr-5 text-white text-4xl p-5">
+          АВТОМАТИЗАЦИЯ АПК
+          </div>
+        <hr className="mx-auto w-4/5 text-center color-white"/>
+          <div className="font-extralight text-white text-2xl m-3">
+          Компания TerraFarm также работает в сфере автоматизации готовых решений. Мы готовы взяться за автоматизацию Ваших АПК под ключ.
+          </div>
+        </div>
+
       </>
     );
 }
